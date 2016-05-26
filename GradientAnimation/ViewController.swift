9296d9e9f234d1ccff3050a4a93c5199a4850eaf
Fabian Langlet <fabianlanglet@Fabians-MacBook-Pro.local>
2016-05-26 12:21:14 +0200

    Comment programmatically instanciate

--- GradientAnimation/ViewController.swift
+++ GradientAnimation/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let img = LoadingImageView(image: UIImage(named: "bulb"))
+        //let img = LoadingImageView(image: UIImage(named: "bulb"))
     }
 
     override func didReceiveMemoryWarning() {
